74f05c9186133aa46bb07d4b3372a5874743700f
Tony Rivera <tonyrivera@users.noreply.github.com>
2018-10-18 21:34:13 -0700

    Minimal server that passes data

--- HTTPServer/Program.cs
+++ HTTPServer/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Net;
+using System.Text;
 
 namespace HTTPServer
 {
-    class Program
+    class Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:1337/");
+
+            listener.Start();
+            Console.WriteLine("Server started: http://localhost:1337/");
+
+            const string html = "<b>Test</b>";
+            byte[] data = Encoding.UTF8.GetBytes(html);
+
+            // HTTP Server Loop
+            while(true){
+
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerResponse response = context.Response;
+                response.OutputStream.Write(data);
+                response.Close();
+            }
         }
     }
 }
