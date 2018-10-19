cebb8f88c0c14b866d10ec01720c49b8a62c6f99
Tony Rivera <tonyrivera@users.noreply.github.com>
2018-10-18 20:44:25 -0700

    Initial commit

---
+++ HTTPServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HTTPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
