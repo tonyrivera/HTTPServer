4a47a6fd52b14ab4d8536a790e1c080d854ec42d
Tony Rivera <tonyrivera@users.noreply.github.com>
2018-10-18 22:15:30 -0700

    Add server config vars

--- HTTPServer/Program.cs
+++ HTTPServer/Program.cs
@@ -8,20 +8,23 @@ namespace HTTPServer
     {
         static void Main(string[] args)
         {
-        
-            HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://localhost:1337/");
-
-            listener.Start();
-            Console.WriteLine("Server started: http://localhost:1337/");
-
+            // Config
+            const string URI = "http://localhost:";
+            const int PORT = 1337;
             const string html = "<b>Test</b>";
             byte[] data = Encoding.UTF8.GetBytes(html);
+
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(URI + PORT.ToString() + "/");
+
+            listener.Start();
+            Console.WriteLine("Server started on " + URI + PORT.ToString());
 
             // HTTP Server Loop
             while(true){
 
                 HttpListenerContext context = listener.GetContext();
+                // HttpListenerRequest request = context.Request;
                 HttpListenerResponse response = context.Response;
                 response.OutputStream.Write(data);
                 response.Close();
